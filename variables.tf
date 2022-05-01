0bfd5d705996b2f99bfd16febac3bc8dd9460cdc
svk1427 <75059407+svk1427@users.noreply.github.com>
2022-05-01 11:36:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "region" {
   default = "us-east-1"
 }
-variable "vpc_cidr" {
-  default = "10.20.0.0/16"
-}
+# variable "vpc_cidr" {
+#   default = "10.20.0.0/16"
+# }
 
 variable "tenancy" {
   default = "default"
